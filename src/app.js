364b9a14ecdcd710b63f6e38f42dc4bf3c91a9aa
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-10-07 15:42:11 +0300

    Task 1

---
+++ src/app.js
@@ -0,0 +1,58 @@
+// sources: see file README.md
+
+const requestBody = {
+  "query": [
+      {
+          "code": "Vuosi",
+          "selection": {
+              "filter": "item",
+              "values": [
+                  "2000", "2001", "2002", "2003", "2004", 
+                  "2005", "2006", "2007", "2008", "2009", 
+                  "2010", "2011", "2012", "2013", "2014", 
+                  "2015", "2016", "2017", "2018", "2019", 
+                  "2020", "2021"
+              ]
+          }
+      },
+      {
+          "code": "Alue",
+          "selection": {
+              "filter": "item",
+              "values": [
+                  "SSS"  
+              ]
+          }
+      },
+      {
+          "code": "Tiedot",
+          "selection": {
+              "filter": "item",
+              "values": [
+                  "vaesto" 
+              ]
+          }
+      }
+  ],
+  "response": {
+      "format": "json-stat2" 
+  }
+};
+
+const url = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"; 
+fetch(url, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(requestBody)
+}).then((response) => {
+  if (!response.ok) {
+    throw new Error(`Error fetching population data. Response status: ${response.status}`);
+  }
+  return response.json();
+}).then((data) => {
+  console.log(data);
+}).catch(error => {
+    console.error("Error fetching population data:", error);
+});
