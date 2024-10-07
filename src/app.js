e7dd28218163c216575e01f94891f6fe8f1521cb
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-10-07 18:29:07 +0300

    Task 3: added search functionality

--- src/app.js
+++ src/app.js
@@ -39,8 +39,31 @@ const requestBody = {
   }
 };
 
-const url = "https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px"; 
-fetch(url, {
+// get municipality codes and names
+let municipalityCodes = [];
+let municipalityNames = [];
+fetch("https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px", {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json"
+  }
+}).then((response) => {
+  if (!response.ok) {
+    throw new Error(`Error fetching municipality data. Response status: ${response.status}`);
+  }
+  return response.json();
+}).then(data => {
+  console.log(data); 
+  municipalityCodes = data.variables[1].values; 
+  municipalityNames = data.variables[1].valueTexts; 
+  console.log(municipalityCodes); 
+  console.log(municipalityNames);
+}).catch(error => {
+  console.error("Error fetching municipality data:", error); 
+});
+
+// fetch data
+fetch("https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px", {
   method: "POST",
   headers: {
     "Content-Type": "application/json"
@@ -72,4 +95,66 @@ fetch(url, {
   });
 }).catch(error => {
   console.error("Error fetching population data:", error);
+});
+
+document.getElementById("input-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const inputArea = document.getElementById("input-area").value.trim().toLowerCase();
+  
+  // search for the matching municipality code
+  let matchedCode = null;
+  if (!inputArea) {
+    matchedCode = "SSS";
+    console.log(`Municipality code for whole country is ${matchedCode}`)
+  } else {
+    for (let i = 0; i < municipalityNames.length; i++) {
+      if (municipalityNames[i].toLowerCase() === inputArea) {
+        matchedCode = municipalityCodes[i];
+        break;
+      }
+    }
+    if (!matchedCode) {
+      alert("Municipality not found. Please try again.");
+      return;
+    }
+    console.log(`Municipality code for ${inputArea} is ${matchedCode}`)
+  }
+  
+  // modify the POST request body
+  requestBody.query[1].selection.values = [matchedCode];
+
+  // fetch population data of the selected municipality
+  fetch("https://statfin.stat.fi/PxWeb/api/v1/en/StatFin/synt/statfin_synt_pxt_12dy.px", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(requestBody)
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error(`Error fetching population data. Response status: ${response.status}`);
+    }
+    return response.json();
+  }).then(data => {
+    const chartData = {
+      labels: Object.values(data.dimension.Vuosi.category.label), 
+      datasets: [
+        {
+          name: "Population",
+          values: data.value  
+        }
+      ]
+    }
+    // create a chart
+    const chart = new frappe.Chart("#chart", {
+      title: "Population Growth in Finland (2000-2021)",
+      data: chartData,
+      type: 'line',
+      height: 450,  
+      colors: ['#eb5146'] 
+    });
+  })
+  .catch(error => {
+    console.error("Error fetching population data:", error);
+  });
 });
